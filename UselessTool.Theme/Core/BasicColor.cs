b52eb5a28f77bd806a447979a24fab0cb185c1c9
Mioter <1833274584@qq.com>
2025-01-10 13:52:13 +0800

    颜色更改功能基本实现，其他未测试。

--- UselessTool.Theme/Core/BasicColor.cs
+++ UselessTool.Theme/Core/BasicColor.cs
@@ -5,25 +5,11 @@ namespace UselessTool.Theme.Core;
 
 public static class BasicColor
 {
-    
     /// <summary>
     /// 预设颜色名与画刷键值对。
     /// </summary>
-    public static IEnumerable<ColorBrushPair> Brushes { get; } =
+    public static IEnumerable<object> Brushes { get; } =
         typeof(Colors)
             .GetProperties(BindingFlags.Public | BindingFlags.Static)
-            .Select(pi => new ColorBrushPair(pi.Name, new SolidColorBrush((Color)pi.GetValue(null)!)));
-
-
-    /// <summary>
-    /// 颜色名与画刷键值对。
-    /// </summary>
-    /// <param name="name">颜色名</param>
-    /// <param name="brush">画刷</param>
-    public class ColorBrushPair(string name, SolidColorBrush brush)
-    {
-        public string Name { get; set; } = name;
-        public SolidColorBrush Brush { get; set; } = brush;
-    }
-
+            .Select(pi => new { pi.Name, Brush = new SolidColorBrush((Color)pi.GetValue(null)!) });
 }
